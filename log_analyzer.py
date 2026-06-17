f324ef8fa511d308b9bbcaa6bda054b7464bcf7c
Mohamed Adnan <adnanamadou07@icloud.com>
2026-06-17 17:24:53 +0100

    added Command-Line Arguments & Error Handling

--- log_analyzer.py
+++ log_analyzer.py
@@ -1,4 +1,8 @@
-with open("sample.log", "r", encoding="utf-8") as f:
-    reader = f
-    for i in reader:
-        print(i.strip())
+import sys
+try:
+    with open(sys.argv[1], "r", encoding="utf-8") as f:
+        for i in f:
+            print(i.strip())
+
+except FileNotFoundError:
+    print("Error: The file 'filename' does not exist.")
