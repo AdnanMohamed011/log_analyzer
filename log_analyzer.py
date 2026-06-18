ed6e99ff329be08c58194a388e5020bc6dd77a4c
Mohamed Adnan <adnanamadou07@icloud.com>
2026-06-18 20:59:35 +0100

    added Filtering by Keyword, Counting Errors & Warnings

--- log_analyzer.py
+++ log_analyzer.py
@@ -1,8 +1,24 @@
 import sys
 try:
     with open(sys.argv[1], "r", encoding="utf-8") as f:
+        error = 0
+        warning = 0
         for i in f:
-            print(i.strip())
-
+            line = i.lower()
+            if "error" in line:
+                error += 1
+            if "warn" in line:
+                warning += 1
+            if len(sys.argv) > 2:
+                if sys.argv[-1].lower() in line:
+                    print(i.strip())
+        
+            elif len(sys.argv) == 2:
+                print(i.strip())
+        print(f"""
+--- Analysis Summary ---
+Total Errors: {error}
+Total Warnings: {warning}
+""")
 except FileNotFoundError:
-    print("Error: The file 'filename' does not exist.")
+    print(f"Error: The file '{sys.argv[1]}' does not exist.")
