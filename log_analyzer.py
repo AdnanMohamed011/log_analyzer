229344bcdbf6d56415b2cd7094cdd9db3b1adf08
Mohamed Adnan <adnanamadou07@icloud.com>
2026-06-21 17:40:45 +0100

    polished and finished the project

--- log_analyzer.py
+++ log_analyzer.py
@@ -1,24 +1,48 @@
-import sys
-try:
-    with open(sys.argv[1], "r", encoding="utf-8") as f:
-        error = 0
-        warning = 0
-        for i in f:
-            line = i.lower()
-            if "error" in line:
-                error += 1
-            if "warn" in line:
-                warning += 1
-            if len(sys.argv) > 2:
-                if sys.argv[-1].lower() in line:
+import argparse
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(description="Analyze the logfile")
+    parser.add_argument("logfile")
+    parser.add_argument("--keyword", "-k")
+    parser.add_argument("--date", "-d")
+    args = parser.parse_args()
+
+    logfile = args.logfile
+    keyword = args.keyword
+    date = args.date
+    return logfile, keyword, date
+
+def analyze_logs(logfile, keyword, date):
+    try:
+        with open(logfile, "r", encoding="utf-8") as f:
+            error = 0
+            warning = 0
+            for i in f:
+                line = i.lower()
+                should_print = True
+
+                if keyword and keyword.lower() not in line:
+                    should_print = False
+                if date and date not in line:
+                    should_print = False
+                
+                if should_print == True:
                     print(i.strip())
-        
-            elif len(sys.argv) == 2:
-                print(i.strip())
-        print(f"""
---- Analysis Summary ---
-Total Errors: {error}
-Total Warnings: {warning}
-""")
-except FileNotFoundError:
-    print(f"Error: The file '{sys.argv[1]}' does not exist.")
+                
+                if "error" in line:
+                    error += 1
+                if "warn" in line:
+                    warning += 1
+
+            print(f"""
+    --- Analysis Summary ---
+    Total Errors: {error}
+    Total Warnings: {warning}
+    """)
+    except FileNotFoundError as e:
+        print(f"Error: The file '{logfile}' does not exist: {e}")
+
+if __name__ == "__main__":
+    logfile, keyword, date = parse_arguments()
+    analyze_logs(logfile, keyword, date)
+    pass
